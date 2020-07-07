551794b3dc6c6cc62f911e7d0e500e58512c5f37
rusefi <rusefillc@gmail.com>
2020-07-06 21:35:43 -0400

    DFU file read fix

--- src/test/java/com/rusefi/dfu/DfuReaderSandbox.java
+++ src/test/java/com/rusefi/dfu/DfuReaderSandbox.java
@@ -1,7 +1,9 @@
 package com.rusefi.dfu;
 
+import java.io.IOException;
+
 public class DfuReaderSandbox {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DfuImage dfuImage = new DfuImage();
         dfuImage.read("rusefi.dfu");
     }
